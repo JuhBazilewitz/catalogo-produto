c14a88728a45c8a1832ad7011f7028b3998d5139
E-UNICRED\juliane.bazilewitz <juliane.bazilewitz@unicred.com.br>
2018-07-14 09:22:00 -0300

    pratica servlet e com demais tecnologias explicadas

---
+++ src/br/com/praticaweb/model/Fabricante.java
@@ -0,0 +1,28 @@
+package br.com.praticaweb.model;
+
+/**
+ * 
+ * @author juliane.bazilewitz
+ *
+ */
+public class Fabricante {
+
+    private Integer codigo;
+    private String nome;
+    
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/br/com/praticaweb/model/Produto.java
@@ -0,0 +1,46 @@
+package br.com.praticaweb.model;
+
+/**
+ * 
+ * @author juliane.bazilewitz
+ *
+ */
+public class Produto {
+
+    private Integer codigo;
+    private String nome;
+    private double valor;
+    private Fabricante fabricante;
+    
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+}
